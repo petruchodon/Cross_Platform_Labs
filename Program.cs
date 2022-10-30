1955edc568765cc5789ae8a06715affab612bc3f
petruchodon <55779535+petruchodon@users.noreply.github.com>
2022-10-30 10:01:17 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.IO;
+
+public static class Rectangles
+{
+    
+    internal static void Main()
+    {
+        
+        long w;
+        long h;
+        w = long.Parse(Console.ReadLine());
+        h = long.Parse(Console.ReadLine());
+        long s = 0;
+        for (long i = 1; i != w + 1; ++i)
+        {
+            for (long j = 1; j != h + 1; ++j)
+            {
+                s += i * j;
+            }
+        }
+        Console.Write(s);
+        
+    }
+    
+
+}
